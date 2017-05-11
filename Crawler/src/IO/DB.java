786762b4c30246791403b3fed6a6aec29b8ff718
Mateusz Kupper <mkupper@agencyport.com>
2017-05-11 14:33:16 -0400

    Cache used instead of a db for links queue

--- Crawler/src/IO/DB.java
+++ Crawler/src/IO/DB.java
@@ -1,24 +1,34 @@
 package IO;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import page.UnvisitedPage;
 import page.VisitedPage;
 
 public class DB {
 	static ConnectionDetails connDetails;
-	
+	static List<String> linksCache;
 	public static UnvisitedPage queryUnvisited() throws Exception {
 		try {
-			Connection con = getConnection();
-			Statement stmt;
-			stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery(XMLRetriever.getQuery("QueryToRetrieveRandomUnvisitedPage"));
-			while(rs.next())  
-				return new UnvisitedPage(rs.getString(2), rs.getInt(1));
-			con.close(); 
-		} catch (SQLException e) {
-			e.printStackTrace();
+//			Connection con = getConnection();
+//			Statement stmt;
+//			stmt = con.createStatement();
+//			ResultSet rs = stmt.executeQuery(XMLRetriever.getQuery("QueryToRetrieveRandomUnvisitedPage"));
+//			while(rs.next())  
+//				return new UnvisitedPage(rs.getString(2), rs.getInt(1));
+//			con.close(); 
+			
+		//} catch (SQLException e) {
+		//	e.printStackTrace();
+			Random generator = new Random();
+			Object[] values = linksCache.toArray();
+			String randomValue = (String) values[generator.nextInt(values.length)];
+			return new UnvisitedPage(randomValue, 0);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -39,6 +49,8 @@ public class DB {
 	
 	static {
 		try {
+			linksCache = new ArrayList<String>();
+			linksCache.add("https://en.wikipedia.org/wiki/Belgium_national_football_team");
 			connDetails = XMLRetriever.retrieveDBDetails();
 		} catch(Exception e) { 
 			e.printStackTrace();
@@ -79,15 +91,16 @@ public class DB {
 	public static void addUnvisited(List<String> links) throws Exception {
 		try {
 			for(String link : links) {
-				Connection con = getConnection();
-				PreparedStatement stmt;
-				stmt = con.prepareStatement(XMLRetriever.getQuery("QueryToAddUnvisitedPages"));
-				stmt.setString(1, link);			
-				stmt.executeUpdate();
-				con.close(); 				
+//				Connection con = getConnection();
+//				PreparedStatement stmt;
+//				stmt = con.prepareStatement(XMLRetriever.getQuery("QueryToAddUnvisitedPages"));
+//				stmt.setString(1, link);			
+//				stmt.executeUpdate();
+//				con.close();
+				linksCache.add(link);
 			} 
-		} catch (SQLException e) {
-			e.printStackTrace();
+		//} catch (SQLException e) {
+			//e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
